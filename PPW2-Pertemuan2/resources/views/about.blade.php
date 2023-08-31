6fe592a66e7f3e4d2c9b0f796c41863783c754e2
imrismaa <rismasaputri@mail.ugm.ac.id>
2023-08-31 14:05:00 +0700

    update about.blade.php

--- PPW2-Pertemuan2/resources/views/about.blade.php
+++ PPW2-Pertemuan2/resources/views/about.blade.php
@@ -8,8 +8,8 @@
 </head>
 <body>
     <h1>Halaman About</h1>
-    <h1> <?= $name; ?> </h1>
-    <h1> <?= $email; ?> </h1>
+    <h1> {{ $name }} </h1>
+    <h1> {{ $email }} </h1>
 <script src="js/script.js"></script>
 </body>
 </html>
